a5e436d0c56c660f24ed4144e48d94c164580bda
Daviks <fedaloor_0605@hotmail.es>
2013-07-27 22:18:14 -0500

    Inserts de Cuentas por Pagar

---
+++ Cuentas por Pagar/inserts frecuencia medio impuesto.sql
@@ -0,0 +1,35 @@
+use TECAv8
+go
+
+insert into CuentaxPagar.FrecuenciaPago  values(1,'Dias', '2013-07-27',1,1,1)
+go
+insert into CuentaxPagar.FrecuenciaPago  values(2,'Semanas', '2013-07-27',1,1,1)
+go
+insert into CuentaxPagar.FrecuenciaPago  values(3,'Meses', '2013-07-27',1,1,1)
+go
+insert into CuentaxPagar.FrecuenciaPago  values(4,'Semestres', '2013-07-27',1,1,1)
+go
+insert into CuentaxPagar.FrecuenciaPago  values(5,'Años', '2013-07-27',1,1,1)
+go
+
+insert into CuentaxPagar.MedioPago  values(1,'Efectivo', 2013-07-27,1,1,1)
+go
+insert into CuentaxPagar.MedioPago  values(2,'Cheque', '2013-07-27',1,1,1)
+go
+insert into CuentaxPagar.MedioPago  values(3,'Tarjeta Credito', '2013-07-27',1,1,1)
+go
+insert into CuentaxPagar.MedioPago  values(4,'Transferencia', '2013-07-27',1,1,1)
+go
+insert into CuentaxPagar.MedioPago  values(5,'Bien Mueble', '2013-07-27',1,1,1,1)
+go
+
+insert into CuentaxPagar.Impuesto  values(1,'IVA 0%', 0.00,'2013-07-27',1,1,1)
+go
+insert into CuentaxPagar.Impuesto  values(2,'IVA 12%', 12.00,'2013-07-27',1,1,1)
+go
+insert into CuentaxPagar.Impuesto  values(3,'RETENCION 1%', 1,00, '2013-07-27',1,1,1)
+go
+insert into CuentaxPagar.Impuesto  values(4,'RETENCION 2%', 2,00, '2013-07-27',1,1,1)
+go
+insert into CuentaxPagar.Impuesto  values(5,'RETENCION 8%', 8,00, '2013-07-27',1,1,1)
+go
