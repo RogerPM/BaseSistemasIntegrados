b9ff9053139e3fd78ad2837b829c1ad7267bc6d1
Daviks <fedaloor_0605@hotmail.es>
2013-08-31 03:48:14 -0500

    Corrección de inserts

--- Cuentas por Pagar/inserts frecuencia medio impuesto.sql
+++ Cuentas por Pagar/inserts frecuencia medio impuesto.sql
@@ -12,7 +12,7 @@ go
 insert into CuentaxPagar.FrecuenciaPago  values(5,'Años', '2013-07-27',1,1,1)
 go
 
-insert into CuentaxPagar.MedioPago  values(1,'Efectivo', 2013-07-27,1,1,1)
+insert into CuentaxPagar.MedioPago  values(1,'Efectivo', '2013-07-27',1,1,1)
 go
 insert into CuentaxPagar.MedioPago  values(2,'Cheque', '2013-07-27',1,1,1)
 go
@@ -20,16 +20,27 @@ insert into CuentaxPagar.MedioPago  values(3,'Tarjeta Credito', '2013-07-27',1,1
 go
 insert into CuentaxPagar.MedioPago  values(4,'Transferencia', '2013-07-27',1,1,1)
 go
-insert into CuentaxPagar.MedioPago  values(5,'Bien Mueble', '2013-07-27',1,1,1,1)
+insert into CuentaxPagar.MedioPago  values(5,'Bien Mueble', '2013-07-27',1,1,1)
 go
 
 insert into CuentaxPagar.Impuesto  values(1,'IVA 0%', 0.00,'2013-07-27',1,1,1)
 go
 insert into CuentaxPagar.Impuesto  values(2,'IVA 12%', 12.00,'2013-07-27',1,1,1)
 go
-insert into CuentaxPagar.Impuesto  values(3,'RETENCION 1%', 1,00, '2013-07-27',1,1,1)
+insert into CuentaxPagar.Impuesto  values(3,'RETENCION 1%', 1.00, '2013-07-27',1,1,1)
 go
-insert into CuentaxPagar.Impuesto  values(4,'RETENCION 2%', 2,00, '2013-07-27',1,1,1)
+insert into CuentaxPagar.Impuesto  values(4,'RETENCION 2%', 2.00, '2013-07-27',1,1,1)
 go
-insert into CuentaxPagar.Impuesto  values(5,'RETENCION 8%', 8,00, '2013-07-27',1,1,1)
+insert into CuentaxPagar.Impuesto  values(5,'RETENCION 8%', 8.00, '2013-07-27',1,1,1)
+go
+
+insert into CuentaxPagar.EmpresaServicio values('0927771895001', '2013-07-27', 'Claro', 'Guayaquil', 'asda', 'asdafdf', 1, 1, 1, 1)
+go
+insert into CuentaxPagar.EmpresaServicio values('0982561267001', '2013-07-27', 'Movistar', 'Guayaquil', 'asda', 'asdafdf', 1, 1, 1, 1)
+go
+insert into CuentaxPagar.EmpresaServicio values('0982571267001', '2013-07-27', 'CNT', 'Guayaquil', 'asda', 'asdafdf', 1, 1, 1, 1)
+go
+insert into CuentaxPagar.EmpresaServicio values('0982561217001', '2013-07-27', 'TVCable', 'Guayaquil', 'asda', 'asdafdf', 1, 1, 1, 1)
+go
+insert into CuentaxPagar.EmpresaServicio values('0982531267001', '2013-07-27', 'Transporte ejecutivo', 'Guayaquil', 'asda', 'asdafdf', 1, 1, 1, 1)
 go
